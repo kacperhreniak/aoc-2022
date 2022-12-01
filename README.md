13578a11a2daa3189de9a0e25552d401a199747b
Kacper Hreniak <kacper.hreniak@allegro.pl>
2022-12-01 11:05:27 +0100

    Day 1 - improvement

--- README.md
+++ README.md
@@ -1,25 +1,13 @@
-# aoc-2022
+# Advent of Code 2022
 
 Welcome to the Advent of Code[^aoc] Kotlin project created by [kacperhreniak][github] using the [Advent of Code Kotlin Template][template] delivered by JetBrains.
 
 In this repository, kacperhreniak is about to provide solutions for the puzzles using [Kotlin][kotlin] language.
 
-If you're stuck with Kotlin-specific questions or anything related to this template, check out the following resources:
+Solutions for [Advent of Code 2022](https://adventofcode.com/2022).
 
-- [Kotlin docs][docs]
-- [Kotlin Slack][slack]
-- Template [issue tracker][issues]
+## Puzzles
 
-
-[^aoc]:
-    [Advent of Code][aoc] – An annual event of Christmas-oriented programming challenges started December 2015.
-    Every year since then, beginning on the first day of December, a programming puzzle is published every day for twenty-five days.
-    You can solve the puzzle and provide an answer using the language of your choice.
-
-[aoc]: https://adventofcode.com
-[docs]: https://kotlinlang.org/docs/home.html
-[github]: https://github.com/kacperhreniak
-[issues]: https://github.com/kotlin-hands-on/advent-of-code-kotlin-template/issues
-[kotlin]: https://kotlinlang.org
-[slack]: https://surveys.jetbrains.com/s3/kotlin-slack-sign-up
-[template]: https://github.com/kotlin-hands-on/advent-of-code-kotlin-template
+| Day | Puzzle | Language | Solution |
+| --: | :----- | :------- | :------- |
+| 1 | [Calorie Counting](https://adventofcode.com/2022/day/1) | C++ | [day1](src/day1) |
--- src/day1/Day01.kt
+++ src/day1/Day01.kt
@@ -1,51 +1,51 @@
 import java.util.PriorityQueue
 import kotlin.math.max
 
-const val MAX_SIZE = 3
+private const val MAX_SIZE = 3
+
+private fun part1(input: List<String>): Int {
+    var max = Int.MIN_VALUE
+    var temp = 0
+
+    input.forEach {
+        if (it.isEmpty() && temp > 0) {
+            max = max(max, temp)
+            temp = 0
+        } else {
+            temp += it.toInt()
+        }
+    }
+
+    return max
+}
+
+private fun part2(input: List<String>): Int {
+    fun PriorityQueue<Int>.addNewItem(value: Int) {
+        if (isNotEmpty() && size == MAX_SIZE && peek() < value) poll()
+        if (value != 0) add(value)
+    }
+
+    val queue = PriorityQueue<Int>(MAX_SIZE, compareByDescending { it })
+    var temp = 0
+
+    input.forEach {
+        if (it.isEmpty() && temp > 0) {
+            queue.addNewItem(value = temp)
+            temp = 0
+        } else {
+            temp += it.toInt()
+        }
+    }
+    queue.addNewItem(value = temp)
+
+    var result = 0
+    for (index in 0 until MAX_SIZE) {
+        result += queue.poll()
+    }
+    return result
+}
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        var max = Int.MIN_VALUE
-        var temp = 0
-        for (line in input) {
-            if (line == "" && temp > 0) {
-                max = max(max, temp)
-                temp = 0
-            } else {
-                temp += line.toInt()
-            }
-        }
-        return max
-    }
-
-    fun part2(input: List<String>): Int {
-        fun PriorityQueue<Int>.addNewItem(value: Int) {
-            if(isNotEmpty() && size == MAX_SIZE && peek() < value){
-                poll()
-            }
-            if(value != 0) add(value)
-        }
-
-        val queue = PriorityQueue<Int>(MAX_SIZE, compareBy { it })
-        var temp = 0
-        for (line in input) {
-            if (line == "" && temp > 0) {
-                queue.addNewItem(value = temp)
-                temp = 0
-            } else {
-                temp += line.toInt()
-            }
-        }
-
-        queue.addNewItem(value = temp)
-
-        var result = 0
-        for(index in 0 until MAX_SIZE){
-            result += queue.poll()
-        }
-        return result
-    }
-
     val input = readInput("day1/input")
     println(part1(input))
     println(part2(input))
