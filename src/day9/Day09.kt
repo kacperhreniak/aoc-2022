97123194cad41b0e87d9dfcbdcbebfd54b9d393a
Kacper Hreniak <kacper.hreniak@allegro.pl>
2022-12-08 21:00:55 +0100

    prepare day 9

---
+++ src/day9/Day09.kt
@@ -0,0 +1,19 @@
+package day9
+
+import readInput
+
+private fun part1(input: List<String>): Int {
+    var result = 0
+    return result
+}
+
+private fun part2(input: List<String>): Int {
+    var result = 0
+    return result
+}
+
+fun main() {
+    val input = readInput("day9/test-input")
+    println(part1(input))
+    println(part2(input))
+}
